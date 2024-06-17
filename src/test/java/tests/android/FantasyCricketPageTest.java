bdd1367bea45ff01d4dbb26bd0fa3bc58f4c8f83
swisshakya47 <shakya.swechchya47@gmail.com>
2024-06-17 10:05:45 +0545

    Page test updated

--- src/test/java/tests/android/FantasyCricketPageTest.java
+++ src/test/java/tests/android/FantasyCricketPageTest.java
@@ -1,63 +1,65 @@
 package tests.android;
 
 import appiumtests.BaseTest;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import pages.android.FantasyPage;
+import pages.android.FantasyCricketPage;
 import pages.android.WelcomePage;
 import utils.Util;
 
-import java.util.concurrent.TimeUnit;
-
-public class FantasyPageTest extends BaseTest {
+public class FantasyCricketPageTest extends BaseTest {
     WelcomePage welcome;
-    FantasyPage fantasy;
+    FantasyCricketPage fantasy;
     Util util;
 
     @BeforeClass
-    public void setUpJutPattiSinglePlayer() {
+    public void setUpCricketFantasyPage() {
         welcome = new WelcomePage(driver);
-        fantasy = new FantasyPage(driver);
+        fantasy = new FantasyCricketPage(driver);
         util= new Util(driver);
     }
 
-    @Test
+    @Test(description = "Verify onboarding process")
+
     public void setWelcomeScreen() throws InterruptedException {
-        Thread.sleep(1000);
+
         welcome.clickSkipButton();
         Thread.sleep(1000);
         welcome.clickPlayAsGuest();
         Thread.sleep(1000);
         welcome.clickTapHereToSkip();
+        Thread.sleep(1000);
     }
 
-    @Test
-    public void ActionForCricketFantasyPage() throws InterruptedException {
+    @Test(description = "Click fantasy icon and link account")
 
-        Thread.sleep(3000);
+    public void fantasyIconAndSignInWithGoogle() throws InterruptedException {
+
         fantasy.clickFantasyPageIcon();
-        Thread.sleep(1000);
         fantasy.clickSignInWithGoogle();
-        Thread.sleep(1000);
         fantasy.clickChooseAnAccount();
-        Thread.sleep(1000);
         fantasy.clickStartPlayingBtn();
-        Thread.sleep(1000);
         fantasy.clickAllowBtn();
-        Thread.sleep(6000);
-        try {
-            fantasy.clickMyMatches();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-//        fantasy.clickMyMatches();
+    }
 
+    @Test(description = "click my matches and join a match button")
+
+    public void myMatchesAction() throws InterruptedException {
+        Thread.sleep(5000);
+        fantasy.clickMyMatches();
         Thread.sleep(1000);
         fantasy.clickJoinAMatchBtn();
         Thread.sleep(1000);
+    }
+
+
+
+    @Test(description = "Verify flow for leaderboard action")
+
+    public void leaderboardAction() throws InterruptedException {
+        Thread.sleep(3000);
         fantasy.clickLeaderBoard();
         Thread.sleep(1000);
         fantasy.clickPastWeekLeftArrow();
@@ -66,9 +68,25 @@ public class FantasyPageTest extends BaseTest {
         Thread.sleep(1000);
         fantasy.clickNextWeekRightArrow();
         Thread.sleep(2000);
+
+    }
+
+    @Test(description = "Action for exit app")
+
+    public void exitAppAction() throws InterruptedException {
         fantasy.clickCrossSymbol();
-        fantasy.clickYesExitBtn();
-        Thread.sleep(3000);
+        Thread.sleep(1000);
+        try {
+            Assert.assertEquals(fantasy.verifyExitAppTest(),"Are you sure you want to exit?Exiting will take you back to Bhoos Games app.");
+        }
+        catch (AssertionError e){
+            throw e;
+        }
+        finally {
+            Thread.sleep(1000);
+            fantasy.clickYesExitBtn();
+            Thread.sleep(3000);
+        }
     }
 }
 
