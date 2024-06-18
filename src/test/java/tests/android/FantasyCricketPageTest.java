129fc5c1a309ffd8f73c9c3ea7bad9946e624b40
swisshakya47 <shakya.swechchya47@gmail.com>
2024-06-18 14:26:17 +0545

    assertion check

--- src/test/java/tests/android/FantasyCricketPageTest.java
+++ src/test/java/tests/android/FantasyCricketPageTest.java
@@ -7,27 +7,25 @@ import org.testng.annotations.Test;
 
 import pages.android.FantasyCricketPage;
 import pages.android.WelcomePage;
-import utils.Util;
+
 
 public class FantasyCricketPageTest extends BaseTest {
     WelcomePage welcome;
     FantasyCricketPage fantasy;
-    Util util;
+
 
     @BeforeClass
     public void setUpCricketFantasyPage() {
         welcome = new WelcomePage(driver);
         fantasy = new FantasyCricketPage(driver);
-        util= new Util(driver);
+
     }
 
     @Test(description = "Verify onboarding process")
 
     public void setWelcomeScreen() throws InterruptedException {
 
-        welcome.clickSkipButton();
-        Thread.sleep(1000);
-        welcome.clickPlayAsGuest();
+
         Thread.sleep(1000);
         welcome.clickTapHereToSkip();
         Thread.sleep(1000);
@@ -47,17 +45,43 @@ public class FantasyCricketPageTest extends BaseTest {
     @Test(description = "click my matches and join a match button")
 
     public void myMatchesAction() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         fantasy.clickMyMatches();
+    }
+
+    @Test(description = "Checking text for the my matches page")
+
+    public void myMatchesText1() throws InterruptedException {
+        Thread.sleep(2000);
+        try {
+            Assert.assertEquals(fantasy.getMyMatchesText1(), "Whoops! You haven't joined any cricket matches");
+        }
+        catch (AssertionError e){
+            throw e;
+        }
+    }
+
+
+
+    @Test(description ="Checking text for my matches page")
+
+    public void myMatchesText2() throws InterruptedException {
         Thread.sleep(1000);
-        fantasy.clickJoinAMatchBtn();
-        Thread.sleep(1000);
+        try {
+            Assert.assertEquals(fantasy.getMyMatchesText2(), "Try joining a new match in the explore page.");
+        }
+        catch (AssertionError e){
+            throw e;
+        }
+        finally {
+            fantasy.clickJoinAMatchBtn();
+            Thread.sleep(1000);
+        }
     }
 
 
 
     @Test(description = "Verify flow for leaderboard action")
-
     public void leaderboardAction() throws InterruptedException {
         Thread.sleep(3000);
         fantasy.clickLeaderBoard();
@@ -68,21 +92,40 @@ public class FantasyCricketPageTest extends BaseTest {
         Thread.sleep(1000);
         fantasy.clickNextWeekRightArrow();
         Thread.sleep(2000);
-
     }
 
-    @Test(description = "Action for exit app")
-
-    public void exitAppAction() throws InterruptedException {
-        fantasy.clickCrossSymbol();
+    @Test(description = "Verify text for leaderboard page")
+    public void leaderboardText1() throws InterruptedException{
         Thread.sleep(1000);
         try {
-            Assert.assertEquals(fantasy.verifyExitAppTest(),"Are you sure you want to exit?Exiting will take you back to Bhoos Games app.");
+            Assert.assertEquals(fantasy.getLeaderboardText1(),"Stay tuned for the next week's leaderboards.");
         }
         catch (AssertionError e){
             throw e;
         }
-        finally {
+
+    }
+
+    @Test(description = "Verify text for leaderboard page")
+    public void leaderboardText2() throws InterruptedException{
+        Thread.sleep(1000);
+        try {
+            Assert.assertEquals(fantasy.getLeaderboardText2(),"Get ready to compete in our next week's weekly leaderboard");
+        }
+        catch (AssertionError e){
+            throw e;
+        }
+
+    }
+
+    @Test(description = "Action for exit app")
+    public void exitAppAction() throws InterruptedException {
+        Thread.sleep(1000);
+        fantasy.clickCrossSymbol();
+        Thread.sleep(2000);
+        try {
+            Assert.assertEquals(fantasy.getExitAppText(),"Are you sure you want to exit?Exiting will take you back to Bhoos Games app.");
+        } finally {
             Thread.sleep(1000);
             fantasy.clickYesExitBtn();
             Thread.sleep(3000);
