35672a630779ba5844ebcbcd05c353d63cbe25a3
swisshakya47 <shakya.swechchya47@gmail.com>
2024-06-18 14:25:25 +0545

    Assertion check

--- src/main/java/pages/android/FantasyCricketPage.java
+++ src/main/java/pages/android/FantasyCricketPage.java
@@ -12,15 +12,22 @@ public class FantasyCricketPage extends BasePage {
     private static final By chooseAnAccount = AppiumBy.xpath("(//android.widget.LinearLayout[@resource-id=\"com.google.android.gms:id/container\"])[4]");
     private static final By startPlayingBtn = AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"Start Playing!\"]/android.view.ViewGroup/android.view.View");
     private static final By allowBtn = AppiumBy.xpath("//android.widget.Button[@resource-id=\"com.android.permissioncontroller:id/permission_allow_button\"]");
-    private static final By myMatches = AppiumBy.xpath("//android.view.View[@content-desc=\"My Matches\"]/android.view.View");
+    private static final By myMatches = AppiumBy.xpath("//android.view.View[@content-desc=\"My Matches\"]");
+    //Assertion for My matches page text
+    private static final By myMatchesText1 = AppiumBy.xpath("//android.widget.TextView[@text=\"Whoops! You haven't joined any cricket matches\"]");
+    private static final By myMatchesText2 = AppiumBy.xpath("////android.widget.TextView[@text=\"Try joining a new match in the explore page.\"]");
     private static final By joinAMatchBtn = AppiumBy.xpath("//android.widget.Button[@text=\"JOIN A MATCH\"]");
     private static final By leaderBoard = AppiumBy.xpath("//android.view.View[@content-desc=\"Leaderboard\"]/android.view.View");
     private static final By pastWeekLeftArrow = AppiumBy.xpath("//android.view.View[@content-desc=\"pastweek\"]/android.widget.Image");
     private static final By currentWeekRightArrow = AppiumBy.xpath("//android.view.View[@content-desc=\"nextweek\"]/android.widget.Image");
     private static final By nextWeekRightArrow = AppiumBy.xpath("//android.view.View[@content-desc=\"nextweek\"]/android.widget.Image");
+    //Assertion for leaderboard page text
+    private static final By leaderBoardText1 = AppiumBy.xpath("//android.widget.TextView[@text=\"Stay tuned for the next week's leaderboards.\"]");
+    private static final By leaderBoardText2 = AppiumBy.xpath("//android.widget.TextView[@text=\"Get ready to compete in our next week's weekly leaderboard.\"]");
     private static final By crossSymbol = AppiumBy.xpath("//android.widget.Button[@text=\"svg%3e\"]");
+    //Assertion for exit app page text
     private static final By exitAppText= AppiumBy.xpath("//android.widget.TextView[@text=\"Are you sure you want to exit? Exiting will take you back to Bhoos Games app.\"]");
-    private static final By yesExitBtn = AppiumBy.xpath("//android.widget.Button[@text=\"YES, EXIT\"]");
+    private static final By yesExitBtn = AppiumBy.xpath("//android.widget.Button[@text=\'YES, EXIT\']");
 
 
 
@@ -53,8 +60,15 @@ public class FantasyCricketPage extends BasePage {
         Thread.sleep(1000);
         click(myMatches);
     }
+    public String getMyMatchesText1(){
+        return getText(myMatchesText1);
+    }
 
-    public void clickJoinAMatchBtn() {
+    public String getMyMatchesText2(){
+        return getText(myMatchesText2);
+    }
+
+    public void clickJoinAMatchBtn(){
         click(joinAMatchBtn);
     }
 
@@ -73,11 +87,18 @@ public class FantasyCricketPage extends BasePage {
     public void clickNextWeekRightArrow() {
         click(nextWeekRightArrow);
     }
+    public String getLeaderboardText1(){
+        return getText(leaderBoardText1);
+    }
 
-    public void clickCrossSymbol() {
+    public String getLeaderboardText2(){
+        return getText(leaderBoardText2);
+    }
+
+    public void clickCrossSymbol(){
         click(crossSymbol);
     }
-    public String verifyExitAppTest(){
+    public String getExitAppText(){
         return getText(exitAppText);
     }
 
