b45734fd28a29c46150a7a41a0569f852dbfda26
swisshakya47 <shakya.swechchya47@gmail.com>
2024-06-26 10:25:01 +0545

    Completed cricket fantasy

--- src/main/java/pages/android/KittiSinglePlayerGamePage.java
+++ src/main/java/pages/android/KittiSinglePlayerGamePage.java
@@ -8,9 +8,9 @@ import org.openqa.selenium.By;
 public class KittiSinglePlayerGamePage extends BasePage {
     private static final By kittiIcon = AppiumBy.xpath("//android.view.ViewGroup[@resource-id=\"game-tile-kitti-test-id\"]/android.widget.ImageView");
     private static final By kittiSinglePlayerIcon = AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"Single Player, Level up your skills\"]/android.view.ViewGroup[2]");
-    private static final By showIcon = AppiumBy.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[62]/android.view.ViewGroup/android.widget.ImageView");
+    private static final By showIcon = AppiumBy.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[61]/android.view.ViewGroup/android.widget.ImageView");
     private static final By kittiSHamBurgerIcon = AppiumBy.xpath("//android.view.ViewGroup[@resource-id=\"in-game-hamburger-id\"]/android.widget.ImageView");
-    private static final By saveAndQuit = AppiumBy.xpath("//android.view.ViewGroup[@resource-id='save-and-quit-test-id']");
+    private static final By saveAndQuit = AppiumBy.xpath("//android.view.ViewGroup[@resource-id=\"save-and-quit-test-id\"]");
 
     public KittiSinglePlayerGamePage(AppiumDriver driver) {
         super(driver);
